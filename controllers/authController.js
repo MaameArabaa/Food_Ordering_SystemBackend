5fc054ded69c297a5c715f8ba40fd5c23909df11
Arabam <arabaamoah1612@gmail.com>
2024-12-13 19:12:24 +0000

    Addition of password validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,11 +4,17 @@ const Login = async (req,res)=>{
     const {fullname, password} = req.body
     console.log(`Fullname: ${fullname} || password: ${password}`)
 
-    const {rows} = await pool.query('SELECT * FROM users WHERE fullname = $1', [fullname])
+    const results = await pool.query('SELECT * FROM users WHERE fullname = $1', [fullname])
     
-    if(rows.length<1){
+    if(results.rows.length<1){
         console.log("No information found in our database")
         return res.status(404).send("No user found")
+    }
+
+    let user = results.rows[0]
+    if(user.password !== password){
+        console.log("Invaild credentials")
+        return res.status(400).send("Invalid credentials")
     }
 
     console.log(`User ${fullname} logged in succesfully`)
